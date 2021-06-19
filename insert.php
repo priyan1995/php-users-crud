1e0b10263104055ecd409347a9d7f0b560ef7197
priyan <45042030+priyan1995@users.noreply.github.com>
2021-06-19 22:17:24 +0530

    submit class added

--- insert.php
+++ insert.php
@@ -2,6 +2,10 @@
 require_once __DIR__ . "/header.php";
 require_once __DIR__ . "/connection.php";
 
+$obj = new Connection();
+
+
+
 ?>
 
 
---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php 
+ 
+ require_once __DIR__.'/connection.php';
+ 
+
+
+ class Submit{
+
+    public function saveStudent($name,$age,$subject){
+        $dbConnect = new Connection();
+        $conn = $dbConnect->connect();
+        $data = mysqli_query($conn, "insert into students values('','".$name."','".$age."','".$subject."')");
+        return $data;
+        if($data){
+            echo "data added";
+        }
+    } 
+
+ }
+
+// $submitdata = new Submit();
+
+// $submitdata->saveStudent('John','20','Maths');
+
+
+
+
+
+
+?>
