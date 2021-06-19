7efb433055b71d35afb8e73a5d4971ef00bbeb2b
priyan <45042030+priyan1995@users.noreply.github.com>
2021-06-19 23:40:12 +0530

    crud function added

--- insert.php
+++ insert.php
@@ -1,9 +1,25 @@
 <?php
 require_once __DIR__ . "/header.php";
 require_once __DIR__ . "/connection.php";
+require_once __DIR__ . "/submit.php";
 
-$obj = new Connection();
+$connection = new Connection();
+$submitData = new Submit();
 
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $namerec = $_POST['name'];
+    $agerec = $_POST['age'];
+    $subjectrec = $_POST['subject'];
+
+
+    if(isset($namerec) && isset($agerec) && isset($subjectrec) ){
+        $submitData-> saveStudent($namerec,$agerec,$subjectrec);
+    }
+
+}
 
 
 ?>
@@ -11,27 +27,74 @@ $obj = new Connection();
 
 <section class="insert-form-sec">
     <div class="container">
-        <form>
+        <form id="studentAddForm" action="" method="post">
+
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label>Name</label>
+                <input type="text" class="form-control" name="name">
+
             </div>
+
+
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <label>Age</label>
+                <input type="text" class="form-control" name="age">
             </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+
+            <div class="form-group">
+                <label>Subject</label>
+                <input type="text" class="form-control" name="subject">
             </div>
+
             <button type="submit" class="btn btn-primary">Submit</button>
+
         </form>
     </div>
 </section>
 
+<script>
+    // this is the id of the form
+    // $("#studentAddForm").submit(function(e) {
+
+    //     e.preventDefault();
+
+    //     var form = $(this);
+    //     var url = form.attr('action');
+
+    //     $.ajax({
+    //         type: "POST",
+    //         url: url,
+    //         data: form.serialize(), 
+    //         success: function(data) {
+    //             alert(data); 
+    //         }
+    //     });
+
+
+    // });
+
+    $(function() {
+        $("#studentAddForm").submit(function(event) {
+            event.preventDefault();
+
+            // var form = $(this);
+            var url = form.attr('action');
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: $(this).serialize(),
+                success: function(result) {
+                    $("#response").text(result);
+
+                }
+
+            });
+        });
+    });
+</script>
 
 
 
-
-<?php require_once __DIR__.'/footer.php'; ?>
+<?php require_once __DIR__ . '/footer.php'; ?>
