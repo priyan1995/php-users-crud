58017d292eefff101bb800bcb55774800e3394e7
priyan <45042030+priyan1995@users.noreply.github.com>
2021-06-21 00:29:44 +0530

    student single development done

--- students/single.php
+++ students/single.php
@@ -2,7 +2,17 @@
 
 require_once __DIR__ . "../../header.php";
 require_once __DIR__ . "../../classes/connection.php";
-require_once __DIR__ . "../../classes/students/view.php";
+require_once __DIR__ . "../../classes/students/single.php";
+
+$id = $_GET['id'];
+$table = 'students';
+
+
+$student = new ViewStudentSingle();
+
+$student_name = $student->getstudentname($id, $table);
+$student_age = $student->getstudentAge($id, $table);
+$student_subject = $student->getstudentSubject($id, $table);
 
 
 
@@ -13,10 +23,21 @@ require_once __DIR__ . "../../classes/students/view.php";
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h2>All Students</h2>
+                <h2><?php echo $student_name;  ?></h2>
+                <h4>Age: <?php echo $student_age; ?></h4>
+                <h4>Subject: <?php echo $student_subject; ?></h4>
+
             </div>
 
-          
+
+            <div class="col-12 text-center">
+                <a href="view.php" class="btn btn-primary">All Students</a>
+                <a href="edit.php" class="btn btn-success">Edit Students</a>
+                <a class="btn btn-danger">Delete Students</a>
+
+
+            </div>
+
         </div>
     </div>
 </section>
